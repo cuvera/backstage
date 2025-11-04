7667b3fb4b04117e6fb850127f8f3136b67954f2
tapan2502 <tapanupa4321@gmail.com>
2025-11-04 15:33:42 +0530

    deleteing

--- app/api/v1/endpoints/router.py
+++ app/api/v1/endpoints/router.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
 from .health import router as health_router
-from .painpoint_analytics import router as painpoint_analytics_router
 
 api_router = APIRouter()
 api_router.include_router(health_router, prefix="/health", tags=["health"])
-api_router.include_router(painpoint_analytics_router, tags=["Painpoint Analytics"])
